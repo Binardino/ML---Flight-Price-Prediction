22b68eb439c2769ed0f9eeb88e019ed7d0a31dd4
Binardino <langlois.robin@gmail.com>
2026-02-17 19:20:20 +0100

    feat: add load_model & basic variables

--- st_dashboard.py
+++ st_dashboard.py
@@ -9,3 +9,21 @@ st.set_page_config(page_title='Flight Price Predictor',
                    layout='wide')
 
 @st.cache_resource
+def load_model(model_path):
+    if not model_path.exists():
+        st.error('Model not found. Please export first a trained model')
+        st.stop()
+
+    return joblib.load(model_path)
+
+
+model_path = Path('artifacts') / 'flight_price_model.joblib'
+model = load_model(model_path)
+
+#%%
+# defining variables - list & default var
+AIRLINES            = ["IndiGo", "Air India", "Vistara", "SpiceJet", "GO FIRST", "AirAsia"]
+CITIES              = ["Delhi", "Mumbai", "Bangalore", "Kolkata", "Hyderabad", "Chennai"]
+TIME_BANDS          = ["Early_Morning", "Morning", "Afternoon", "Evening", "Night", "Late_Night"]
+STOPS_LABEL_TO_NUM  = {"zero": 0, "one": 1, "two_or_more": 2}
+CLASS_LABEL_TO_NUM  = {"Economy": 0, "Business": 1}    
